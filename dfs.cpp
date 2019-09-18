522cd6e401da80a0a7a3f4657841e9c3954a711d
Muhammad Sarmad <49847242+msarmad17@users.noreply.github.com>
2019-09-17 23:27:51 -0400

    Add files via upload

---
+++ dfs.cpp
@@ -0,0 +1,104 @@
+#include <vector>
+#include <queue>
+#include <stack>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/*
+bfs
+{
+    load start to q
+    mark start as disc
+
+    while q not empty
+        de-q v
+        en-q undisc Nbors of v, check if any of them is G
+        if q size equals 1 and no Nbors in prev step, give up
+        mark v exp
+        mark v's Nbors as disc
+}
+*/
+
+int N[2][4];
+
+
+struct node
+{
+    int x;
+    int y;
+    int val;
+    int disc;
+    int exp;
+};
+
+stack<struct node> trav;
+stack<struct node> path;
+
+int dfs(struct node * v)
+{
+    (*v).disc = 1;
+
+    N[0][0] = (*v).x - (*v).val;
+    N[0][1] = (*v).x + (*v).val;
+    N[0][2] = (*v).x;
+    N[0][3] = (*v).x;
+
+    N[0][0] = (*v).y;
+    N[1][0] = (*v).y;
+    N[2][0] = (*v).y - (*v).val;
+    N[3][0] = (*v).y + (*v).val;
+
+    trav.push(*v);
+
+    for(int i = 0; i < 4; i++)
+    {
+        if(N[i][0] >= 0 && N[i][1] >= 0 && N[i][0] <= r && N[i][1] <= c)
+        {
+            if( !((maze[(N[i][0])][(N[i][1])]).exp) )
+                if( (maze[(N[i][0])][(N[i][1])]).val != 0)
+                    dfs( &(maze[(N[i][0])][(N[i][1])]) );
+                else
+                {
+                    trav.push((maze[(N[i][0])][(N[i][1])]));
+                    return 0;
+                }
+        }
+    (*v).exp = 1;
+    }
+}
+
+
+int main(int argc, char * argv[])
+{
+    public int r, c;
+
+    ifstream in;
+
+    in.open("in.txt");
+
+    in >> r >> c;
+
+    cout << r << " " << c << endl;
+
+    public struct node maze[r][c];
+
+    for(int i = 0; i < r; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            (maze[i][j]).x = i;
+            (maze[i][j]).y = j;
+            in >> (maze[i][j]).val;
+            (maze[i][j]).disc = 0;
+            (maze[i][j]).exp = 0;
+        }
+    }
+
+    int ch = 0;
+    struct node * temp;
+    dfs(&(maze[0][0]));
+
+
+}
---
+++ maze.cpp
@@ -0,0 +1,155 @@
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+/*
+bfs
+{
+    load start to q
+    mark start as disc
+
+    while q not empty
+        de-q v
+        en-q undisc neighbors of v, check if any of them is G
+        if q size equals 1 and no neighbors in prev step, give up
+        mark v exp
+        mark v's neighbors as disc
+}
+*/
+
+
+struct node
+{
+    int x;
+    int y;
+    int val;
+    int disc;
+    int exp;
+    string route;
+    //char ro[32];
+};
+
+string north = "N\0";
+string south = "S\0";
+string east = "E\0";
+string west = "W\0";
+
+
+int main(int argc, char * argv[])
+{
+    int r, c;
+
+    ifstream in;
+
+    in.open("input.txt");
+
+    in >> r >> c;
+
+    //cout << r << " " << c << endl;
+
+    struct node maze[r][c];
+
+    for(int i = 0; i < r; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            (maze[i][j]).x = i;
+            (maze[i][j]).y = j;
+            in >> (maze[i][j]).val;
+            (maze[i][j]).disc = 0;
+            (maze[i][j]).exp = 0;
+            //((maze[i][j]).route).erase( ((maze[i][j]).route).begin(), ( (maze[i][j]).route ).end() );
+            (maze[i][j]).route = "\0";
+        }
+    }
+
+    in.close();
+
+    struct node *temp;
+    queue<struct node> trav;
+
+    trav.push((maze[0][0]));
+    (maze[0][0]).disc = 1;
+    //(maze[0][0]).route = "";
+    //(maze[0][0]).route = "\0";
+
+
+
+    while(!trav.empty())
+    {
+        temp = &(trav.front());
+
+        //cout << (*temp).route << endl;
+
+        string str = temp->route + '\0';
+
+        trav.pop();
+
+
+        if( (*temp).x == r-1 && (*temp).y == c-1 )
+        {
+            //cout << str << endl;
+
+            ofstream out;
+
+            out.open("output.txt");
+
+            out << str;
+
+            out.close();
+
+            //ch = 1;
+            break;
+        }
+
+        //cout << *temp.x << "," << *temp.y << endl;
+
+        if((*temp).x - (*temp).val >= 0) //up
+            if( !((maze[(*temp).x - (*temp).val][(*temp).y]).disc) )
+            {
+                (maze[(*temp).x - (*temp).val][(*temp).y]).disc = 1;
+                (maze[(*temp).x - (*temp).val][(*temp).y]).route = str + north + '\0';
+                //strcpy( (maze[(*temp).x - (*temp).val][(*temp).y]).ro, (*temp).ro);
+                //(maze[(*temp).x - (*temp).val][(*temp).y]).ro[ sizeof((maze[(*temp).x - (*temp).val][(*temp).y]).ro) ] = 'N';
+                trav.push( (maze[(*temp).x - (*temp).val][(*temp).y]) );
+                //cout << (maze[(*temp).x - (*temp).val][(*temp).y]).route << endl;
+            }
+
+        if((*temp).x + (*temp).val <= r-1) //down
+            if( !((maze[(*temp).x + (*temp).val][(*temp).y]).disc) )
+            {
+                (maze[(*temp).x + (*temp).val][(*temp).y]).disc = 1;
+                (maze[(*temp).x + (*temp).val][(*temp).y]).route = str + south + '\0';
+                trav.push( (maze[(*temp).x + (*temp).val][(*temp).y]) );
+                //cout << (maze[(*temp).x + (*temp).val][(*temp).y]).route << endl;
+            }
+
+        if((*temp).y - (*temp).val >= 0) //left
+            if( !((maze[(*temp).x][(*temp).y - (*temp).val]).disc) )
+            {
+                 (maze[(*temp).x][(*temp).y - (*temp).val]).disc = 1;
+                 (maze[(*temp).x][(*temp).y - (*temp).val]).route = str + west + '\0';
+                 trav.push( (maze[(*temp).x][(*temp).y - (*temp).val]) );
+                 //cout << (maze[(*temp).x][(*temp).y - (*temp).val]).route << endl;
+            }
+
+        if((*temp).y + (*temp).val <= c-1) //right
+            if( !((maze[(*temp).x][(*temp).y + (*temp).val]).disc) )
+            {
+                (maze[(*temp).x][(*temp).y + (*temp).val]).disc = 1;
+                (maze[(*temp).x][(*temp).y + (*temp).val]).route = str + east + '\0';
+                trav.push( (maze[(*temp).x][(*temp).y + (*temp).val]) );
+                //cout << (maze[(*temp).x][(*temp).y + (*temp).val]).route << endl;
+            }
+
+        (*temp).exp = 1;
+
+    }
+
+
+}
+
